eb684b591c4ef879c80be47272d0ddeceaa44440
Aarifm <Aarifm>
2019-01-07 15:35:59 +0530

    added server

---
+++ src/server.js
@@ -0,0 +1,5 @@
+const express = require('express')
+let app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.static('src'));
+app.listen(PORT,function(){'node app is running on port',PORT});
